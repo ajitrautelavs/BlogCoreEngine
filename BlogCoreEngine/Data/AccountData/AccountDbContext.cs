d3397f1fbba31984ddd2effd061bd3e2bb8563e2
Ajit Rautela <ajitr@acs-australia.com.au>
2022-09-07 10:27:53 +1000

    Add project files.

---
+++ BlogCoreEngine/Data/AccountData/AccountDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogCoreEngine.Data.AccountData
+{
+    public class AccountDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AccountDbContext(DbContextOptions<AccountDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
